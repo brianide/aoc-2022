4de18c372787573288d40c92635e7969deb236a2
brianide <brianide@gmail.com>
2022-12-08 00:32:11 -0600

    Day 8 gold

--- Advent2022/src/Day8.fs
+++ Advent2022/src/Day8.fs
@@ -5,9 +5,14 @@ open Util.Extensions
 open Util.Patterns
 
 let parse (file: string) =
-    file
-    |> File.ReadAllLines
-    |> Array.map (Seq.map (fun n -> int n - int '0') >> Seq.toArray)
+    let table =
+        file
+        |> File.ReadAllLines
+        |> Array.map (Seq.map (fun n -> int n - int '0') >> Seq.toArray)
+
+    let height = Array.length table
+    let width = Array.length table[0]
+    (table, width, height)
 
 let genLines width height = seq {
     for y in 1 .. height - 2 do
@@ -22,9 +27,7 @@ let genLines width height = seq {
 }
 
 let solveSilver file =
-    let table = parse file
-    let height = Array.length table
-    let width = Array.length table[0]
+    let (table, width, height) = parse file
 
     let folder (maximum, visible) (x, y) = 
         let curr = table[y][x]
@@ -39,8 +42,39 @@ let solveSilver file =
     |> (+) 4
     |> string
 
+let genCoords width height = seq {
+    for y in 0 .. height - 1 do
+        for x in 0 .. width - 1 do
+            yield (x, y)
+}
+
+let genScenicLines width height x y = seq {
+    yield [x + 1 .. width - 1] |> List.map (fun dx -> (dx, y))
+    yield [x - 1 .. -1 .. 0] |> List.map (fun dx -> (dx, y))
+    yield [y + 1 .. height - 1] |> List.map (fun dy -> (x, dy))
+    yield [y - 1 .. -1 .. 0] |> List.map (fun dy -> (x, dy))
+}
+
 let solveGold file =
-    parse file |> ignore
-    ""
+    let (table, width, height) = parse file
+
+    let scoreLine cent line =
+        line
+        |> Seq.map (fun (x, y) -> table[y][x])
+        |> Seq.tryFindIndex (fun n -> n >= cent)
+        |> function
+        | Some n -> n + 1
+        | None -> Seq.length line
+
+    let scoreTree (x, y) =
+        let cent = table[y][x]
+
+        genScenicLines width height x y
+        |> Seq.map (scoreLine cent)
+
+    genCoords width height
+    |> Seq.map (scoreTree >> Seq.reduce (*))
+    |> Seq.max
+    |> string
 
 let Solvers = {| Gold = solveGold; Silver = solveSilver |}
