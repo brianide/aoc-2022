6d53b2dbe03856592aa5d6c3655797f24cf503db
brianide <brianide@gmail.com>
2022-12-09 22:57:52 -0600

    Refactoring

--- src/Program.fs
+++ src/Program.fs
@@ -3,15 +3,16 @@
 let getSolver day part =
     let day =
         match day with
-        | 1 -> Day1.Solvers
-        | 2 -> Day2.Solvers
-        | 3 -> Day3.Solvers
-        | 4 -> Day4.Solvers
-        | 5 -> Day5.Solvers
-        | 6 -> Day6.Solvers
-        | 7 -> Day7.Solvers
-        | 8 -> Day8.Solvers
-        | 9 -> Day9.Solvers
+        | "1" -> Day1.Solvers
+        | "2" -> Day2.Solvers
+        | "3" -> Day3.Solvers
+        | "4" -> Day4.Solvers
+        | "5" -> Day5.Solvers
+        | "6" -> Day6.Solvers
+        | "7" -> Day7.Solvers
+        | "8" -> Day8.Solvers
+        | "9" -> Day9.Solvers
+        | "9m" -> Day9Mutable.Solvers
         | _ -> failwith "Invalid day"
     
     match part with
@@ -23,6 +24,6 @@ let getSolver day part =
 [<EntryPoint>]
 let main args =
     match args with
-    | [|Int32 day; inputPath; part|] -> getSolver day part inputPath |> printfn "%s"
+    | [| day; inputPath; part|] -> getSolver day part inputPath |> printfn "%s"
     | _ -> failwithf "Invalid arg string: %A" args
     0
