6d2cd94d5ff906ab2c7f18b8fbc70af209fb60b8
brianide <brianide@gmail.com>
2022-12-10 00:11:26 -0600

    Washed ass

--- src/Day10.fs
+++ src/Day10.fs
@@ -5,59 +5,54 @@ open System.IO
 open Util.Extensions
 open Util.Patterns
 
-type Instruction =
-| ADD of int
-| NOP
+let Dims = {|Width = 40; Height = 6|}
 
-let parse (file: string) =
+let parse file =
     let parseLine line =
         match String.split " " line with
-        | ["addx"; Int32 x] -> seq {NOP; ADD x}
-        | ["noop"] -> Seq.singleton NOP
+        | ["addx"; Int32 x] -> [0; x]
+        | ["noop"] -> [0]
         | _ -> failwithf "Invalid instruction: %s" line
 
     File.ReadAllLines file |> Seq.collect parseLine
 
-let solveSilver file =
-    let folder (cycle, x, total) inst =
-        let newX =
-            match inst with
-            | ADD n -> x + n
-            | NOP -> x
+let runProgram prog =
+    let mutable cycle = 1
+    let mutable x = 1
+    let mutable sigTotal = 0
+    let display = BitArray(Dims.Width * Dims.Height)
 
+    let tick n =
         if (cycle - 20) % 40 = 0 then
-            printfn "%A" x
-            (cycle + 1, newX, total + x * cycle)
-        else
-            (cycle + 1, newX, total)
+            sigTotal <- sigTotal + x * cycle
 
+        let crt = cycle - 1
+        display[crt] <- abs (crt % Dims.Width - x) <= 1
+
+        x <- x + n
+        cycle <- cycle + 1
+
+    Seq.iter tick prog
+    (sigTotal, display)
+
+
+let solveSilver file =
     parse file
-    |> Seq.fold folder (1, 1, 0)
+    |> runProgram
+    |> fst
     |> string
 
 let solveGold file =
-    let mutable pos = 1
-    let mutable cycle = 1
-    let display = BitArray(40 * 6)
-
-    let tick inst =
-        let crt = cycle - 1
-
-        display[crt] <- abs (crt % 40 - pos) <= 1
-
-        match inst with
-        | ADD n -> pos <- pos + n
-        | _ -> ()
-
-        cycle <- cycle + 1
+    let format (arr: BitArray) =
+        [0 .. arr.Count - 1]
+        |> Seq.map (fun n -> if arr[n] then "#" else " ")
+        |> Seq.chunkBySize Dims.Width
+        |> Seq.map (String.concat "")
+        |> String.concat "\n"
 
     parse file
-    |> Seq.iter tick
-
-    for i in 0 .. 5 do
-        for j in 0 .. 39 do
-            printf "%c" <| if display[i * 40 + j] then '#' else ' '
-        printfn ""
-    ""
+    |> runProgram
+    |> snd
+    |> format
 
 let Solvers = (solveSilver, solveGold)
