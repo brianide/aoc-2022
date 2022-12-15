a255df2f51f55455fab624e82da29c1d0d242de5
brianide <brianide@gmail.com>
2022-12-14 22:54:42 -0600

    Refactoring image output to maybe make it faster

--- src/Image.fs
+++ src/Image.fs
@@ -6,13 +6,13 @@ open System.Text
 
 let saveToPGM byteFn path grid =
     let width, height = Array2D.length2 grid, Array2D.length1 grid
+    let header = $"P5 {width} {height} 255 "
     
-    let header = ASCIIEncoding.ASCII.GetBytes $"P5 {width} {height} 255 "
-    let body = Array.zeroCreate<byte> (header.Length + width * height)
-    Buffer.BlockCopy(header, 0, body, 0, header.Length)
+    let body = Array.zeroCreate<byte> (ASCIIEncoding.ASCII.GetByteCount header + width * height)
+    let offset = ASCIIEncoding.ASCII.GetBytes(header, body)
 
     for i in 0 .. width * height - 1 do
         let r, c = i / width, i % width
-        body[i + header.Length] <- byteFn c r grid[r, c]
+        body[i + offset] <- byteFn c r grid[r, c]
 
     File.WriteAllBytes(path, body)
