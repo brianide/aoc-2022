cbe5e0fc36fa8ff2acd6203d912164468f3a659a
Brian Dixon <brianide@gmail.com>
2022-12-15 08:07:51 -0600

    Hideous day 15 gold

--- src/Day15.fs
+++ src/Day15.fs
@@ -20,6 +20,9 @@ let parse file =
 
 let distance (ax, ay) (bx, by) = abs (bx - ax) + abs (by - ay)
 
+let add (x, y) (i, j) = (x + i, y + j)
+let mul (x, y) n = (x * n, y * n)
+
 // let solveSilver input =
 //     let sensors = Seq.map (fun (spos, bpos) -> spos, distance spos bpos) input
 //     let beacons = Seq.map snd input
@@ -54,18 +57,51 @@ let solveSilver input =
 
 let solveGold input =
     let beacon = (14, 11)
-    let coverage = Seq.mapi (fun i (spos, bpos) -> {|Pos = spos; Range = distance spos bpos; Id = i|}) input
+    let steps = [
+        ((-1, 0), (1, -1))
+        ((0, 1), (1, 1))
+        ((1, 0), (-1, 1))
+        ((0, 1), (-1, -1))
+    ]
 
-    Seq.allPairs coverage coverage
-    |> Seq.filter (fun (a, b) -> a.Id <> b.Id && distance a.Pos b.Pos <= a.Range + b.Range + 2)
-    |> Seq.iter (printfn "%A")
+    // let coverage = Seq.mapi (fun i (spos, bpos) -> {|Pos = spos; Range = distance spos bpos; Id = i|}) input
+    let coverage = Seq.mapi (fun i (spos, bpos) -> (spos, distance spos bpos)) input
+
+    let beacons = input |> Seq.map snd |> Set.ofSeq
+
+    let getHull (pos, range) =
+        let rad = range + 1
+        steps |> Seq.collect (fun (off, step) -> seq {for i in 0 .. range -> mul step i |> add (mul off rad) |> add pos})
+
+    // let (px, py) =
+    //     coverage
+    //     |> Seq.pairs
+    //     |> Seq.filter (fun ((apos, arng), (bpos, brng)) -> (distance apos bpos) - (arng + brng) = 1)
+    //     |> Seq.collect (fun (a, b) -> seq {a; b})
+    //     |> Seq.mapi (fun i s -> (getHull s |> Seq.filter (fun (x, y) -> x >= 0 && x <= 4000000 && y >= 0 && y <= 4000000), Seq.removeAt i coverage))
+    //     |> Seq.choose (fun (hull, others) -> Seq.tryFind (fun p -> Seq.exists (fun (spos, srng) -> distance spos p <= srng) others |> not) hull)
+    //     |> Seq.filter (fun e -> Set.contains e beacons |> not)
+    //     |> Seq.take 1
+    //     |> Seq.exactlyOne
+
+    let (px, py) =
+        coverage
+        |> Seq.mapi (fun i s -> (getHull s |> Seq.filter (fun (x, y) -> x >= 0 && x <= 4000000 && y >= 0 && y <= 4000000), Seq.removeAt i coverage))
+        |> Seq.choose (fun (hull, others) -> Seq.tryFind (fun p -> Seq.exists (fun (spos, srng) -> distance spos p <= srng) others |> not) hull)
+        |> Seq.filter (fun e -> Set.contains e beacons |> not)
+        |> Seq.take 1
+        |> Seq.exactlyOne
+
+    (uint64 px) * 4000000UL + (uint64 py) |> string
+
+    // Seq.allPairs coverage coverage
+    // |> Seq.filter (fun (a, b) -> a.Id <> b.Id && distance a.Pos b.Pos <= a.Range + b.Range + 2)
+    // |> Seq.iter (printfn "%A")
 
     // coverage
     // |> Seq.pairs
     // |> Seq.filter (fun (a, b) -> distance a.Pos b.Pos <= a.Range + b.Range)
     // |> Seq.iter (printfn "%A")
-
-    ""
 
 // let solveGold input =
 //     let beacon = (14, 11)
