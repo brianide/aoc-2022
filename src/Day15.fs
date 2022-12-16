fccf916b41bbd8da7a762d1bbddf56a87523e659
brianide <brianide@gmail.com>
2022-12-15 19:17:09 -0600

    Added comments

--- src/Day15.fs
+++ src/Day15.fs
@@ -23,12 +23,14 @@ let solveSilver input =
     let yval = 2000000
     // let yval = 10
 
+    // Combines sorted ranges
     let rangeFold ranges e =
         match ranges, e with
         | [], e -> [e]
         | (lh, rh) :: more, (le, re) when rh >= le -> (lh, re) :: more
         | ranges, e -> e :: ranges 
 
+    // Find coverage slices
     let ranges =
         input
         |> Seq.map (fun (spos, bpos) -> (spos, distance spos bpos))
@@ -40,6 +42,7 @@ let solveSilver input =
         |> Seq.sortBy fst
         |> Seq.fold rangeFold []
 
+    // Get beacons falling inside the covered ranges
     let beacons =
         input
         |> Seq.map snd
@@ -49,42 +52,49 @@ let solveSilver input =
         |> Seq.filter (fun n -> Seq.exists (fun (lo, hi) -> bounds lo hi n) ranges)
         |> Seq.length
 
+    // Total of the covered ranges, less the number of beacons in the coverage area
     (ranges |> Seq.map (fun (lo, hi) -> hi - lo + 1) |> Seq.sum) - beacons |> string
 
 let solveGold input =
     let limit = 4000000
     // let limit = 20
 
-    let coverage = Seq.map (fun (spos, bpos) -> (spos, distance spos bpos)) input
+    let sensors = Seq.map (fun (spos, bpos) -> (spos, distance spos bpos)) input
     let beacons = input |> Seq.map snd |> Set.ofSeq
 
+    // Converts coordinates to a tilted axis where our diamonds are squares instead
     let tilt (x, y) =
         (x - y, x + y)
 
+    // Unconverts coordinates
     let detilt (a, b) =
         let x = (a + b) / 2
         (x, b - x)
 
+    // Gets the bounds of each sensor's coverage area, expanded by one, in squarespace coordinates
     let squareBounds =
-        Seq.map (fun ((x, y), rng) -> (tilt (x - rng - 1, y), tilt (x + rng + 1, y))) coverage
+        Seq.map (fun ((x, y), rng) -> (tilt (x - rng - 1, y), tilt (x + rng + 1, y))) sensors
 
+    // Find all squarepace X-axis values where two coverage areas are separated by one tile
     let xs =
         squareBounds
         |> Seq.map (Tuple2.map fst)
         |> Seq.fold (fun (acc, bcc) (a, b) -> Set.add a acc, Set.add b bcc) (Set.empty, Set.empty)
         |> fun (a, b) -> Set.intersect a b
 
+    // Same as above but for the Y axis
     let ys =
         squareBounds
         |> Seq.map (Tuple2.map snd)
         |> Seq.fold (fun (acc, bcc) (a, b) -> Set.add a acc, Set.add b bcc) (Set.empty, Set.empty)
         |> fun (a, b) -> Set.intersect a b
 
+    // Pair up coordinates, convert them back into diamondspace, and find the one we want
     Seq.allPairs xs ys
     |> Seq.map detilt
     |> Seq.filter (fun (x, y) -> bounds 0 limit x && bounds 0 limit y)
     |> Seq.filter (fun pc -> not <| Set.contains pc beacons)
-    |> Seq.filter (fun pc -> coverage |> Seq.forall (fun (ps, rng) -> distance pc ps > rng))
+    |> Seq.filter (fun pc -> sensors |> Seq.forall (fun (ps, rng) -> distance pc ps > rng))
     |> Seq.exactlyOne
     |> fun (x, y) -> (uint64 x) * 4000000UL + (uint64 y) |> string
 
