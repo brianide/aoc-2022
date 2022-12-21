8cc8fd437d06dc310966474add5623a12e8faa11
Brian Dixon <brianide@gmail.com>
2022-12-21 03:11:00 -0600

    Refactoring

--- src/Day21.fs
+++ src/Day21.fs
@@ -1,9 +1,7 @@
 module Day21
 
 open System.IO
-open Util.Collections
 open Util.Extensions
-open Util.Math
 open Util.Patterns
 open Util.Plumbing
 
@@ -20,7 +18,15 @@ type Expr =
 | Div of Expr * Expr
 | Equal of Expr * Expr
 
-let parse file =
+let (|BinaryArithmetic|_|) = function
+| Add (a, b) -> Some (Add, (+), a, b)
+| Sub (a, b) -> Some (Sub, (-), a, b)
+| Mul (a, b) -> Some (Mul, (*), a, b)
+| Div (a, b) -> Some (Div, (/), a, b)
+| _ -> None
+
+
+let tokenize file =
     let tokenizeLine = function
     | RegGroups @"([a-z]{4}): ([a-z]{4}) ([*/+-]) ([a-z]{4})" [label; key1; op; key2] -> label, Operation (op, key1, key2)
     | RegGroups @"([a-z]{4}): (\d+)" [label; Int64 value] -> label, Constant value
@@ -28,10 +34,7 @@ let parse file =
 
     File.ReadAllLines file |> Array.map tokenizeLine |> Map.ofArray
 
-let solveSilver input =
-    ""
-
-let rec parseExpression (table: Map<string, Token>) root =
+let rec parseExpression (table: Map<string, Token>) xform root =
     let (|Operator|) = function
     | "+" -> Add
     | "-" -> Sub
@@ -39,53 +42,58 @@ let rec parseExpression (table: Map<string, Token>) root =
     | "/" -> Div
     | op -> failwithf "Invalid operator: %s" op
 
-    match root, table[root] with
-    | "root", Operation (_, key1, key2) -> Equal (parseExpression table key1, parseExpression table key2)
-    | "humn", _ -> Query
-    | _, Constant value -> Const value
-    | _, Operation (Operator op, key1, key2) -> op (parseExpression table key1, parseExpression table key2)
+    match table[root] with
+    | Operation (Operator op, key1, key2) -> op (parseExpression table xform key1, parseExpression table xform key2)
+    | Constant value -> Const value
+    |> fun n -> xform (root, n)
 
 let rec simplify expr =
-    let reduce e = function
-    | Const _ -> failwith "Can't solve constant equality!"
-    | Add (Const n, b) -> Equal (Const (e - n), b)
-    | Add (a, Const n) -> Equal (Const (e - n), a)
-    | Sub (Const n, b) -> Equal (Const (n - e), b)
-    | Sub (a, Const n) -> Equal (Const (e + n), a)
-    | Mul (Const n, b) -> Equal (Const (e / n), b)
-    | Mul (a, Const n) -> Equal (Const (e / n), a)
-    | Div (Const n, b) -> Equal (Const (n / e), b)
-    | Div (a, Const n) -> Equal (Const (e * n), a)
-    | Query -> Const e
-    | x -> printfn "Unreduced: %A" (e, x); x
+    let reduce c = function
+    | Const _ -> failwith "Can't solve equality between constants"
+    | Add (Const n, b) -> Equal (Const (c - n), b) //  c = n + b  ==>  c - n = b
+    | Add (a, Const n) -> Equal (Const (c - n), a) //  c = b + n  ==>  c - n = b
+    | Sub (Const n, b) -> Equal (Const (n - c), b) //  c = n - b  ==>  n - c = b
+    | Sub (a, Const n) -> Equal (Const (c + n), a) //  c = b - n  ==>  c + n = b
+    | Mul (Const n, b) -> Equal (Const (c / n), b) //  c = n * b  ==>  c / n = b
+    | Mul (a, Const n) -> Equal (Const (c / n), a) //  c = b * n  ==>  c / n = b
+    | Div (Const n, b) -> Equal (Const (n / c), b) //  c = n / b  ==>  n / c = b
+    | Div (a, Const n) -> Equal (Const (c * n), a) //  c = b / n  ==>  c * n = b
+    | Query -> Const c
+    | x -> Equal (Const c, x)
 
     match expr with
     | Const n -> Const n
     | Query -> Query
-    | Add (Const a, Const b) -> Const (a + b)
-    | Add (a, b) -> Add (simplify a, simplify b)
-    | Sub (Const a, Const b) -> Const (a - b)
-    | Sub (a, b) -> Sub (simplify a, simplify b)
-    | Mul (Const a, Const b) -> Const (a * b)
-    | Mul (a, b) -> Mul (simplify a, simplify b)
-    | Div (Const a, Const b) -> Const (a / b)
-    | Div (a, b) -> Div (simplify a, simplify b)
+    | BinaryArithmetic (_, fn, Const a, Const b) -> Const (fn a b)
+    | BinaryArithmetic (case, _, a, b) -> case (simplify a, simplify b)
     | Equal (Const a, b) -> reduce a (simplify b)
     | Equal (a, Const b) -> reduce b (simplify a)
     | Equal (a, b) -> Equal (simplify a, simplify b)
+    | ex -> ex
 
+
+let rec simplifyToConstant expr =
+    let next = simplify expr
+    if next <> expr then
+        simplifyToConstant next
+    else
+        match next with
+        | Const n -> n
+        | _ -> failwithf "Expression did not reduce to a constant value: %A" expr
+
+let solveSilver input =
+    parseExpression input snd "root"
+    |> simplifyToConstant
+    |> string
 
 let solveGold input =
-    let rec simp expr =
-        let next = simplify expr
-        if next <> expr then
-            simp next
-        else
-            next
+    let xform = function
+    | "root", BinaryArithmetic (_, _, a, b) -> Equal (a, b)
+    | "humn", _ -> Query
+    | _, n -> n
 
-    parseExpression input "root"
-    |> simp
-    |> printfn "%A"
-    ""
+    parseExpression input xform "root"
+    |> simplifyToConstant
+    |> string
 
-let Solver = chainSolver parse solveSilver solveGold
+let Solver = chainSolver tokenize solveSilver solveGold
